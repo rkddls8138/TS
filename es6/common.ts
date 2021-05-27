f9fc04045cc31ce97b02a92c211943bb55ddfcaf
김강인(Gangin Kim) <gi.kim@konai.com>
2021-05-27 17:40:08 +0900

    asd

---
+++ es6/common.ts
@@ -0,0 +1,13 @@
+const removeHypen = (param:number) => { param.replace(/-/g, '') };
+const removeComma = (param:number) => { param.toString().replace(/,/g, '') };
+
+IntersectionObserver
+
+ResizeObserver: confirm.arguments(true);
+
+class interChangeModel {
+
+    user:String
+
+
+}
